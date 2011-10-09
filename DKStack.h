d5c8a38d32d2b786ae02e7a1eaab1b89970f7384
samyzee <sumeru@ufl.edu>
2011-10-09 02:20:05 -0400

    Added Stack Implementation

---
+++ DKStack.h
@@ -0,0 +1,26 @@
+//
+//  DKStack.h
+//  Additions
+//
+//  Created by Dominik Krejčík on 25/09/2011.
+//  Feel free to use and distribute as long as you mention me. Or buy me a beer.
+//  https://github.com/dominikkrejcik/Objective-C-Stack---Queue
+
+#import <Foundation/Foundation.h>
+
+
+@interface DKStack : NSObject {
+    NSMutableArray* array;
+}
+
+- (NSArray*) objects;
+- (void)pop;
+- (void)push:(id)element;
+- (void)clear;
+
+- (id)top;
++ (DKStack*)stack;
+- (NSInteger)size;
+- (BOOL)isEmpty;
+
+@end
