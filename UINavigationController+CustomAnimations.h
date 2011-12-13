632e35ec187d7c83933c7a2d1222ecddd0fc7030
samyzee <sumeru@ufl.edu>
2011-12-12 20:50:59 -0500

    Added the UINavigation Controller Custom Animations Category

---
+++ UINavigationController+CustomAnimations.h
@@ -0,0 +1,28 @@
+//
+//  UINavigationController+CustomAnimations.h
+//  Additions
+//
+//  Created by Sumeru Chatterjee on 12/12/11.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef enum {
+    UINavigationCustomTransitionStyleZoom = 0,
+    UINavigationCustomTransitionStyleCube,
+    UINavigationCustomTransitionStyleFade,
+    UINavigationCustomTransitionStyleRipple,
+    UINavigationCustomTransitionStyleSuck,
+    UINavigationCustomTransitionStyleCompleteFlip,
+} UINavigationCustomTransitionStyle;
+
+
+@interface UINavigationController (CustomAnimations)
+
+/*Push a view controller with a custom transition style*/
+- (void)pushViewController:(UIViewController *)viewController withCustomTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle;
+
+/* Pop the top the view controller with a custom transition style*/
+- (UIViewController *)popViewControllerwithCustomTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle; 
+
+@end
---
+++ UINavigationController+CustomAnimations.m
@@ -0,0 +1,273 @@
+//
+//  UINavigationController+CustomAnimations.m
+//  Additions
+//
+//  Created by Sumeru Chatterjee on 12/12/11.
+//
+
+#import "UINavigationController+CustomAnimations.h"
+
+//QuartzCore
+#import <QuartzCore/QuartzCore.h>
+
+#define kDefaultTransitionDuration 1
+
+typedef enum {
+	AnimationMethodUIViewAnimation=0,
+	AnimationMethodCATransitionAnimation,
+	AnimationMethodUndefined
+} AnimationMethod;
+
+typedef void(^AnimationBlock)(void);
+typedef void(^CompletionBlock)(BOOL);
+typedef void(^InitializationBlock)(void);
+typedef void(^TransitionBlock)(void);
+
+@interface UINavigationController (Private)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (AnimationMethod) animationMethodForTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle;
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (CFTimeInterval) animationDurationFromTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle;
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (InitializationBlock) viewAnimationInitializationBlockForTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle controller:(UIViewController*)controller forward:(BOOL)forward;
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (AnimationBlock) viewAnimationAnimationBlockForTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle controller:(UIViewController*)controller forward:(BOOL)forward;
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (CompletionBlock) viewAnimationCompletionBlockForTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle controller:(UIViewController*)controller forward:(BOOL)forward;
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (TransitionBlock) transitionBlockForTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle controller:(UIViewController*)controller forward:(BOOL)forward;
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (NSString*)transitionTypeFromTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle;
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (NSString*)transitionForwardSubtypeFromTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle;
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (NSString*)transitionBackwardSubtypeFromTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle;
+
+@end
+
+@implementation UINavigationController (CustomAnimations)
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)pushViewController:(UIViewController *)viewController withCustomTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle 
+{
+    [viewController.view setFrame:self.topViewController.view.frame];
+	if ([self animationMethodForTransitionStyle:transitionStyle]== AnimationMethodUIViewAnimation ) {
+		
+		[self viewAnimationInitializationBlockForTransitionStyle:transitionStyle controller:viewController forward:YES]();
+		[UIView animateWithDuration:[self animationDurationFromTransitionStyle:transitionStyle]
+							  delay:0
+							options:UIViewAnimationOptionCurveEaseInOut
+						 animations:[self viewAnimationAnimationBlockForTransitionStyle:transitionStyle controller:viewController forward:YES]
+						 completion:^(BOOL finished){
+							 [self viewAnimationCompletionBlockForTransitionStyle:transitionStyle controller:viewController forward:YES](finished);
+							 [self pushViewController:viewController animated:NO];
+						 }];
+		
+	} else {
+		[self transitionBlockForTransitionStyle:transitionStyle controller:viewController forward:YES]();
+		[self pushViewController:viewController animated:NO];
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (UIViewController *)popViewControllerwithCustomTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle 
+{
+    UIViewController* returnViewController = nil;
+    
+    UIViewController* viewController = [self.topViewController retain];
+	if ([self animationMethodForTransitionStyle:transitionStyle]== AnimationMethodUIViewAnimation ) {
+		
+		returnViewController = [self popViewControllerAnimated:NO];
+		[self viewAnimationInitializationBlockForTransitionStyle:transitionStyle controller:viewController forward:NO]();
+		[UIView animateWithDuration:[self animationDurationFromTransitionStyle:transitionStyle]
+							  delay:0
+							options:UIViewAnimationOptionCurveEaseInOut
+						 animations:[self viewAnimationAnimationBlockForTransitionStyle:transitionStyle controller:viewController forward:NO]
+						 completion:[self viewAnimationCompletionBlockForTransitionStyle:transitionStyle controller:viewController forward:NO]];
+		
+	} else {
+		[self transitionBlockForTransitionStyle:transitionStyle controller:viewController forward:NO]();
+		returnViewController = [self popViewControllerAnimated:NO];
+	}
+    
+    [viewController release];
+    return returnViewController;
+}
+
+#pragma mark -
+#pragma mark Private Methods
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (AnimationMethod) animationMethodForTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle
+{
+	if (transitionStyle == UINavigationCustomTransitionStyleZoom) {
+		return AnimationMethodUIViewAnimation;
+	}
+	else {
+		return AnimationMethodCATransitionAnimation;
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (CFTimeInterval) animationDurationFromTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle
+{
+	switch (transitionStyle) {
+		case UINavigationCustomTransitionStyleZoom:
+			return 0.4f;
+		case UINavigationCustomTransitionStyleRipple:
+			return 3;
+		default:
+			return kDefaultTransitionDuration;
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Helper Methods for AnimationMethodUIViewAnimation
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+-(InitializationBlock) viewAnimationInitializationBlockForTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle controller:(UIViewController*)controller forward:(BOOL)forward
+{
+	switch (transitionStyle) {
+		case UINavigationCustomTransitionStyleZoom:
+			if (forward) {
+				return [[^{
+					[[self view] addSubview:controller.view];
+					[controller viewWillAppear:YES];
+					controller.view.transform=CGAffineTransformMakeScale(0.001, 0.001);
+				} copy] autorelease];
+				
+			} else {
+				return [[^{
+					[[self view] addSubview:controller.view];
+				} copy] autorelease];
+			}
+			
+			
+		default:
+			return [[^{} copy] autorelease];;
+	}
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+-(AnimationBlock) viewAnimationAnimationBlockForTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle controller:(UIViewController*)controller forward:(BOOL)forward
+{
+	switch (transitionStyle) {
+		case UINavigationCustomTransitionStyleZoom:
+			if (forward) {
+				return [[^{
+					controller.view.transform=CGAffineTransformMakeScale(1, 1);
+				} copy] autorelease];
+				
+			} else {
+				return [[^{
+					controller.view.transform=CGAffineTransformMakeScale(0.001, 0.001);
+				} copy] autorelease];
+			}
+			
+		default:
+			return [[^{} copy] autorelease];
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+-(CompletionBlock) viewAnimationCompletionBlockForTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle controller:(UIViewController*)controller forward:(BOOL)forward
+{
+	switch (transitionStyle) {
+		case UINavigationCustomTransitionStyleZoom:
+			if (forward) {
+				return [[^{
+					[controller.view removeFromSuperview];
+				} copy] autorelease];
+				
+			} else {
+				return [[^{
+					[controller.view removeFromSuperview];
+					controller.view.transform=CGAffineTransformMakeScale(1, 1);
+				} copy] autorelease];
+			}
+		default:
+			return [[^{} copy] autorelease];
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Helper Methods for AnimationMethodCATransitionAnimation
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (TransitionBlock) transitionBlockForTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle controller:(UIViewController*)controller forward:(BOOL)forward;
+{
+	if (forward) {
+		return [[^{
+			CATransition *transition = [CATransition animation];
+			transition.duration = [self animationDurationFromTransitionStyle:transitionStyle];
+			transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+			
+			transition.type = [self transitionTypeFromTransitionStyle:transitionStyle];
+			transition.subtype = [self transitionForwardSubtypeFromTransitionStyle:transitionStyle];
+                    
+			[[self.navigationController.view layer] addAnimation:transition forKey:@"transition"];
+		} copy] autorelease];
+		
+	} else {
+		return [[^{
+			CATransition *transition = [CATransition animation];
+			transition.duration = [self animationDurationFromTransitionStyle:transitionStyle];
+			transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+			
+			transition.type = [self transitionTypeFromTransitionStyle:transitionStyle];
+			transition.subtype =  [self transitionBackwardSubtypeFromTransitionStyle:transitionStyle];
+                    
+			[[self.navigationController.view layer] addAnimation:transition forKey:@"transition"];
+		} copy] autorelease];
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (NSString*)transitionTypeFromTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle
+{
+	switch (transitionStyle) {
+		case UINavigationCustomTransitionStyleCube:
+			return @"cube";
+		case UINavigationCustomTransitionStyleFade:
+			return @"fade";
+		case UINavigationCustomTransitionStyleRipple:
+			return @"rippleEffect";
+		case UINavigationCustomTransitionStyleSuck:
+			return @"suckEffect";
+		case UINavigationCustomTransitionStyleCompleteFlip:
+			return @"flip";
+		default:
+			return nil;
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (NSString*)transitionForwardSubtypeFromTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle
+{
+	switch (transitionStyle) {
+		case UINavigationCustomTransitionStyleCube:
+			return kCATransitionFromBottom;
+		case UINavigationCustomTransitionStyleCompleteFlip:
+			return kCATransitionFromRight;
+		default:
+			return nil;
+	}
+	
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (NSString*)transitionBackwardSubtypeFromTransitionStyle:(UINavigationCustomTransitionStyle)transitionStyle
+{
+	switch (transitionStyle) {
+		case UINavigationCustomTransitionStyleCube:
+			return kCATransitionFromTop;
+		case UINavigationCustomTransitionStyleCompleteFlip:
+			return kCATransitionFromLeft;
+		default:
+			return [self transitionForwardSubtypeFromTransitionStyle:transitionStyle];
+	}
+}
+
+@end
